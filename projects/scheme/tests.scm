780cb4cc6b8b19576078bbf7a44a017cc7b1731e
Jenna Huang <j3nnahuang@berkeley.edu>
2016-03-26 00:37:48 -0700

    please god

---
+++ projects/scheme/tests.scm
@@ -0,0 +1,858 @@
+;;; Test cases for Scheme.
+;;;
+;;; In order to run only a prefix of these examples, add the line
+;;;
+;;; (exit)
+;;;
+;;; after the last test you wish to run.
+
+
+; ###### START OF USER DEFINED TESTS ######
+
+;;; Q1
+
+8
+; expect 8
+
+2.88
+; expect 2.88
+
+'y
+; expect y
+
+;;; Q2
+
+'(1 2 3)
+; expect (1 2 3)
+
+'(1 2 . 3)
+; expect (1 2 . 3)
+
+'(1 2 . (3 4))
+; expect (1 2 3 4)
+
+;;; Q3
+
+-
+; expect #[-]
+
+car
+; expect #[car]
+
+goodbye
+; expect Error: unknown identifier: goodbye
+
+;;; Q4
+
+(+ 2 3 4 5 6)
+; expect 20
+
+(* 5 6)
+; expect 30
+
+(+ (* 2 3) (* 3 4))
+; expect 18
+
+(car (cdr '(3 4 5 6)))
+; expect 4
+
+(integer? 'hello)
+; expect False
+
+;;; Q5A
+
+(define hi 7)
+; expect hi
+
+hi
+; expect 7
+
+(define bye (+ 3 5))
+; expect bye
+
+bye
+; expect 8
+
+(define math (+ hi bye))
+; expect math
+
+math
+; expect 15
+
+;;; Q6B
+
+'(2 . (6))
+; expect (2 6)
+
+(quote (2 . 3))
+; expect (2 . 3)
+
+(cdr '(2 3 4 5))
+; expect (3 4 5)
+
+;;; Q7
+
+(begin 40 '(- 5 2))
+; expect (- 5 2)
+
+(define y (begin (print 10) (* 5 10)))
+; expect 10 ; y
+
+y
+; expect 50
+
+;;; Q8
+
+(lambda (y z) (- y z))
+; expect (lambda (y z) (- y z))
+
+(lambda (x y z) (+ z) (* x y z))
+; expect (lambda (x y z) (+ z) (* x y z))
+
+(lambda () 4)
+; expect (lambda () 4)
+
+;;; Q9A
+
+(define (next-int x) (+ x 1))
+; expect next-int
+
+(next-int 5)
+; expect 6
+
+(define (f) (+ 3 5))
+; expect f
+
+(f)
+; expect 8
+
+;;; Q10 
+
+(define (adder x)
+        (lambda (y) (+ x y)
+        )
+)
+; expect adder
+
+(define make-adder-two (adder 2))
+; make-adder-two
+
+(make-adder-two 5)
+; expect 7
+
+;;; Q11B
+
+(lambda (f f) (/ f f))
+; expect Error
+
+(lambda (y z . x) 'yay)
+; expect Error
+
+(lambda (1 5) (+))
+; expect Error
+
+;;; Q12
+
+(define z 5)
+; expect z
+
+(define (func z) 
+    (lambda (x) (+ z 1))
+)
+; expect func
+
+((func 9) 7)
+; expect 10
+
+
+;;; Q13
+
+(if (= 6 6) 12 8)
+; expect 12
+
+(if False 5 6)
+; expect 6
+
+(if True 'skip-else (/ 1 0))
+; expect skip-else
+
+(if (> 0 5) 'hi)
+; except okay
+
+;;; Q14
+
+(and 5 7 2 #f 0)
+; expect False
+
+(and 1 2 3 4 5)
+; expect 5
+
+(or 1 2 3 4 (/ 1 0))
+; expect 1
+
+(or #f (= 1 0) #f #f 1)
+; expect 1
+
+;;; Q15
+
+(cond ((= 0 0) 'hi)
+      ((> 2 (/ 1 0)) 'bye)
+      (else 'asdfgh)
+)
+; expect hi
+
+(cond ((> 2 5) 'hi)
+      ((= 2 5) 'bye)
+      (else 'i_hate_doctests)
+)
+; expect i_hate_doctests
+
+;;; Q16
+
+(let ((x 5)
+      (y 2))
+     (list (+ x y) (- x y))
+)
+; expect (7 3)
+
+;;; Q17
+
+(define m (mu (a) (/ a b)))
+; expxect m
+
+(define n (lambda (a b c) (m (* a c))))
+; expect n
+
+(n 3 4 8)
+; expect 6
+
+;;; Q22
+
+(define (fact n)
+        (define (fact-iter n prod)
+                (if (= n 0) prod
+                            (fact-iter (- n 1) (* n prod))
+                )
+        )
+    (fact-iter n 1)
+)
+; expect fact
+
+(fact 500)
+; expect 1220136825991110068701238785423046926253574342803192842192413588385845373153881997605496447502203281863013616477148203584163378722078177200480785205159329285477907571939330603772960859086270429174547882424912726344305670173270769461062802310452644218878789465754777149863494367781037644274033827365397471386477878495438489595537537990423241061271326984327745715546309977202781014561081188373709531016356324432987029563896628911658974769572087926928871281780070265174507768410719624390394322536422605234945850129918571501248706961568141625359056693423813008856249246891564126775654481886506593847951775360894005745238940335798476363944905313062323749066445048824665075946735862074637925184200459369692981022263971952597190945217823331756934581508552332820762820023402626907898342451712006207714640979456116127629145951237229913340169552363850942885592018727433795173014586357570828355780158735432768888680120399882384702151467605445407663535984174430480128938313896881639487469658817504506926365338175055478128640000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+
+(define (fib n)
+  (define (fib-help n prev curr)
+          (if (= n 1) curr
+                      (fib-help (- n 1) curr (+ prev curr))
+          )
+  )
+  (fib-help n 0 1)
+)
+; expect fib
+
+(fib 3000)
+; expect 410615886307971260333568378719267105220125108637369252408885430926905584274113403731330491660850044560830036835706942274588569362145476502674373045446852160486606292497360503469773453733196887405847255290082049086907512622059054542195889758031109222670849274793859539133318371244795543147611073276240066737934085191731810993201706776838934766764778739502174470268627820918553842225858306408301661862900358266857238210235802504351951472997919676524004784236376453347268364152648346245840573214241419937917242918602639810097866942392015404620153818671425739835074851396421139982713640679581178458198658692285968043243656709796000
+
+
+
+; ###### END OF USER DEFINED TEST ######
+
+
+
+;;; These are examples from several sections of "The Structure
+;;; and Interpretation of Computer Programs" by Abelson and Sussman.
+
+;;; License: Creative Commons share alike with attribution
+
+;;; 1.1.1
+
+10
+; expect 10
+
+(+ 137 349)
+; expect 486
+
+(- 1000 334)
+; expect 666
+
+(* 5 99)
+; expect 495
+
+(/ 10 5)
+; expect 2
+
+(+ 2.7 10)
+; expect 12.7
+
+(+ 21 35 12 7)
+; expect 75
+
+(* 25 4 12)
+; expect 1200
+
+(+ (* 3 5) (- 10 6))
+; expect 19
+
+(+ (* 3 (+ (* 2 4) (+ 3 5))) (+ (- 10 7) 6))
+; expect 57
+
+(+ (* 3
+      (+ (* 2 4)
+         (+ 3 5)))
+   (+ (- 10 7)
+      6))
+; expect 57
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Move the following (exit) line to run additional tests. ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(exit)
+
+
+;;; 1.1.2
+
+(define size 2)
+; expect size
+size
+; expect 2
+
+(* 5 size)
+; expect 10
+
+(define pi 3.14159)
+(define radius 10)
+(* pi (* radius radius))
+; expect 314.159
+
+(define circumference (* 2 pi radius))
+circumference
+; expect 62.8318
+
+;;; 1.1.4
+
+(define (square x) (* x x))
+; expect square
+(square 21)
+; expect 441
+
+(define square (lambda (x) (* x x))) ; See Section 1.3.2
+(square 21)
+; expect 441
+
+(square (+ 2 5))
+; expect 49
+
+(square (square 3))
+; expect 81
+
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+(sum-of-squares 3 4)
+; expect 25
+
+(define (f a)
+  (sum-of-squares (+ a 1) (* a 2)))
+(f 5)
+; expect 136
+
+;;; 1.1.6
+
+(define (abs x)
+  (cond ((> x 0) x)
+        ((= x 0) 0)
+        ((< x 0) (- x))))
+(abs -3)
+; expect 3
+
+(abs 0)
+; expect 0
+
+(abs 3)
+; expect 3
+
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+(a-plus-abs-b 3 -2)
+; expect 5
+
+;;; 1.1.7
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+(define (improve guess x)
+  (average guess (/ x guess)))
+(define (average x y)
+  (/ (+ x y) 2))
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+(sqrt 9)
+; expect 3.00009155413138
+
+(sqrt (+ 100 37))
+; expect 11.704699917758145
+
+(sqrt (+ (sqrt 2) (sqrt 3)))
+; expect 1.7739279023207892
+
+(square (sqrt 1000))
+; expect 1000.000369924366
+
+;;; 1.1.8
+
+(define (sqrt x)
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
+(sqrt 9)
+; expect 3.00009155413138
+
+(sqrt (+ 100 37))
+; expect 11.704699917758145
+
+(sqrt (+ (sqrt 2) (sqrt 3)))
+; expect 1.7739279023207892
+
+(square (sqrt 1000))
+; expect 1000.000369924366
+
+;;; 1.3.1
+
+(define (cube x) (* x x x))
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+(define (inc n) (+ n 1))
+(define (sum-cubes a b)
+  (sum cube a inc b))
+(sum-cubes 1 10)
+; expect 3025
+
+(define (identity x) x)
+(define (sum-integers a b)
+  (sum identity a inc b))
+(sum-integers 1 10)
+; expect 55
+
+;;; 1.3.2
+
+((lambda (x y z) (+ x y (square z))) 1 2 3)
+; expect 12
+
+(define (f x y)
+  (let ((a (+ 1 (* x y)))
+        (b (- 1 y)))
+    (+ (* x (square a))
+       (* y b)
+       (* a b))))
+(f 3 4)
+; expect 456
+
+(define x 5)
+(+ (let ((x 3))
+     (+ x (* x 10)))
+   x)
+; expect 38
+
+(let ((x 3)
+      (y (+ x 2)))
+  (* x y))
+; expect 21
+
+;;; 2.1.1
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+(define x (cons 1 2))
+(car x)
+; expect 1
+
+(cdr x)
+; expect 2
+
+(define x (cons 1 2))
+(define y (cons 3 4))
+(define z (cons x y))
+(car (car z))
+; expect 1
+
+(car (cdr z))
+; expect 3
+
+z
+; expect ((1 . 2) 3 . 4)
+
+(define (make-rat n d) (cons n d))
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+(define (print-rat x)
+  (display (numer x))
+  (display '/)
+  (display (denom x))
+  (newline))
+(define one-half (make-rat 1 2))
+(print-rat one-half)
+; expect 1/2 ; okay
+
+(define one-third (make-rat 1 3))
+(print-rat (add-rat one-half one-third))
+; expect 5/6 ; okay
+
+(print-rat (mul-rat one-half one-third))
+; expect 1/6 ; okay
+
+(print-rat (add-rat one-third one-third))
+; expect 6/9 ; okay
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (cons (/ n g) (/ d g))))
+(print-rat (add-rat one-third one-third))
+; expect 2/3 ; okay
+
+(define one-through-four (list 1 2 3 4))
+one-through-four
+; expect (1 2 3 4)
+
+(car one-through-four)
+; expect 1
+
+(cdr one-through-four)
+; expect (2 3 4)
+
+(car (cdr one-through-four))
+; expect 2
+
+(cons 10 one-through-four)
+; expect (10 1 2 3 4)
+
+(cons 5 one-through-four)
+; expect (5 1 2 3 4)
+
+(define (map proc items)
+  (if (null? items)
+      nil
+      (cons (proc (car items))
+            (map proc (cdr items)))))
+(map abs (list -10 2.5 -11.6 17))
+; expect (10 2.5 11.6 17)
+
+(map (lambda (x) (* x x))
+     (list 1 2 3 4))
+; expect (1 4 9 16)
+
+(define (scale-list items factor)
+  (map (lambda (x) (* x factor))
+       items))
+(scale-list (list 1 2 3 4 5) 10)
+; expect (10 20 30 40 50)
+
+(define (count-leaves x)
+  (cond ((null? x) 0)
+        ((not (pair? x)) 1)
+        (else (+ (count-leaves (car x))
+                 (count-leaves (cdr x))))))
+(define x (cons (list 1 2) (list 3 4)))
+(count-leaves x)
+; expect 4
+
+(count-leaves (list x x))
+; expect 8
+
+;;; 2.2.3
+
+(define (odd? x) (= 1 (remainder x 2)))
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+(filter odd? (list 1 2 3 4 5))
+; expect (1 3 5)
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+(accumulate + 0 (list 1 2 3 4 5))
+; expect 15
+
+(accumulate * 1 (list 1 2 3 4 5))
+; expect 120
+
+(accumulate cons nil (list 1 2 3 4 5))
+; expect (1 2 3 4 5)
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+(enumerate-interval 2 7)
+; expect (2 3 4 5 6 7)
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+(enumerate-tree (list 1 (list 2 (list 3 4)) 5))
+; expect (1 2 3 4 5)
+
+;;; 2.3.1
+
+(define a 1)
+
+(define b 2)
+
+(list a b)
+; expect (1 2)
+
+(list 'a 'b)
+; expect (a b)
+
+(list 'a b)
+; expect (a 2)
+
+(car '(a b c))
+; expect a
+
+(cdr '(a b c))
+; expect (b c)
+
+(define (memq item x)
+  (cond ((null? x) false)
+        ((eq? item (car x)) x)
+        (else (memq item (cdr x)))))
+(memq 'apple '(pear banana prune))
+; expect False
+
+(memq 'apple '(x (apple sauce) y apple pear))
+; expect (apple pear)
+
+(define (equal? x y)
+  (cond ((pair? x) (and (pair? y)
+                        (equal? (car x) (car y))
+                        (equal? (cdr x) (cdr y))))
+        ((null? x) (null? y))
+        (else (eq? x y))))
+(equal? '(1 2 (three)) '(1 2 (three)))
+; expect True
+
+(equal? '(1 2 (three)) '(1 2 three))
+; expect False
+
+(equal? '(1 2 three) '(1 2 (three)))
+; expect False
+
+;;; Peter Norvig tests (http://norvig.com/lispy2.html)
+
+(define double (lambda (x) (* 2 x)))
+(double 5)
+; expect 10
+
+(define compose (lambda (f g) (lambda (x) (f (g x)))))
+((compose list double) 5)
+; expect (10)
+
+(define apply-twice (lambda (f) (compose f f)))
+((apply-twice double) 5)
+; expect 20
+
+((apply-twice (apply-twice double)) 5)
+; expect 80
+
+(define fact (lambda (n) (if (<= n 1) 1 (* n (fact (- n 1))))))
+(fact 3)
+; expect 6
+
+(fact 50)
+; expect 30414093201713378043612608166064768844377641568960512000000000000
+
+(define (combine f)
+  (lambda (x y)
+    (if (null? x) nil
+      (f (list (car x) (car y))
+         ((combine f) (cdr x) (cdr y))))))
+(define zip (combine cons))
+(zip (list 1 2 3 4) (list 5 6 7 8))
+; expect ((1 5) (2 6) (3 7) (4 8))
+
+(define riff-shuffle (lambda (deck) (begin
+    (define take (lambda (n seq) (if (<= n 0) (quote ()) (cons (car seq) (take (- n 1) (cdr seq))))))
+    (define drop (lambda (n seq) (if (<= n 0) seq (drop (- n 1) (cdr seq)))))
+    (define mid (lambda (seq) (/ (length seq) 2)))
+    ((combine append) (take (mid deck) deck) (drop (mid deck) deck)))))
+(riff-shuffle (list 1 2 3 4 5 6 7 8))
+; expect (1 5 2 6 3 7 4 8)
+
+((apply-twice riff-shuffle) (list 1 2 3 4 5 6 7 8))
+; expect (1 3 5 7 2 4 6 8)
+
+(riff-shuffle (riff-shuffle (riff-shuffle (list 1 2 3 4 5 6 7 8))))
+; expect (1 2 3 4 5 6 7 8)
+
+;;; Additional tests
+
+(apply square '(2))
+; expect 4
+
+(apply + '(1 2 3 4))
+; expect 10
+
+(apply (if false + append) '((1 2) (3 4)))
+; expect (1 2 3 4)
+
+(if 0 1 2)
+; expect 1
+
+(if '() 1 2)
+; expect 1
+
+(or false true)
+; expect True
+
+(or)
+; expect False
+
+(and)
+; expect True
+
+(or 1 2 3)
+; expect 1
+
+(and 1 2 3)
+; expect 3
+
+(and False (/ 1 0))
+; expect False
+
+(and True (/ 1 0))
+; expect Error
+
+(or 3 (/ 1 0))
+; expect 3
+
+(or False (/ 1 0))
+; expect Error
+
+(or (quote hello) (quote world))
+; expect hello
+
+(if nil 1 2)
+; expect 1
+
+(if 0 1 2)
+; expect 1
+
+(if (or false False #f) 1 2)
+; expect 2
+
+(define (loop) (loop))
+(cond (false (loop))
+      (12))
+; expect 12
+
+((lambda (x) (display x) (newline) x) 2)
+; expect 2 ; 2
+
+(define g (mu () x))
+(define (high f x)
+  (f))
+
+(high g 2)
+; expect 2
+
+(define (print-and-square x)
+  (print x)
+  (square x))
+(print-and-square 12)
+; expect 12 ; 144
+
+(/ 1 0)
+; expect Error
+
+(define addx (mu (x) (+ x y)))
+(define add2xy (lambda (x y) (addx (+ x x))))
+(add2xy 3 7)
+; expect 13
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Scheme Implementations ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; len outputs the length of list s
+(define (len s)
+  (if (eq? s '())
+    0
+    (+ 1 (len (cdr s)))))
+(len '(1 2 3 4))
+; expect 4
+
+
+;;;;;;;;;;;;;;;;;;;;
+;;; Extra credit ;;;
+;;;;;;;;;;;;;;;;;;;;
+
+(exit)
+
+; Tail call optimization tests
+
+(define (sum n total)
+  (if (zero? n) total
+    (sum (- n 1) (+ n total))))
+(sum 1001 0)
+; expect 501501
+
+(define (sum n total)
+  (cond ((zero? n) total)
+        (else (sum (- n 1) (+ n total)))))
+(sum 1001 0)
+; expect 501501
+
+(define (sum n total)
+  (begin 2 3
+    (if (zero? n) total
+      (and 2 3
+        (or false
+          (begin 2 3
+            (let ((m n))
+              (sum (- m 1) (+ m total)))))))))
+(sum 1001 0)
+; expect 501501
